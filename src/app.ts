b1cf9680722880c9681b671f224db6589b1f42d8
Leander Doerk <leander@doerk.org>
2024-01-30 21:15:09 +0100

    added JSDoc to the functions off app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ const newWordButton: HTMLElement | null = document.getElementById('new-word');
 class App{
     word: string;
     constructor(){
+        /**
+         * This method adds an event listener to the submitGuess event. When the event is triggered, the guess is checked against the word and the CSS is updated accordingly.
+         */
         addEventListener('submitGuess', (event: any) => {
             const guess: string = event.detail.guess;
             const guessElement: any = document.createElement('guess-component');
@@ -22,10 +25,20 @@ class App{
         
     }
     
+    /**
+     * Selects a word from the wordlist
+     * @param wordlist - The wordlist to select a word from
+     * @returns The selected word
+     */
     selectWord(wordlist: Array<string>): string {
         return wordlist[Math.floor(Math.random() * wordlist.length)];
     }
 
+    /**
+     * This method checks the guess against the word and updates the CSS based on the correct letters.
+     * @param guess The word the user guessed
+     * @param shadow The shadow root of the guess element
+     */
     checkGuess(guess: string, shadow: ShadowRoot): void{
         this.word.split('').forEach((element: string, index: number) => {
             element = element.toUpperCase();
@@ -42,16 +55,29 @@ class App{
         }) 
     };
 
+    /**
+     * This method adds a CSS-class to a letter element at a given index 
+     * @param {number} index - The index of the letter
+     * @param {string} _class - The CSS-class to add (correct/ close/ wrong)
+     * @param {ShadowRoot} shadow - The shadow-root of the guess element
+     */
     addGuessLetterClass(index:number, _class: string, shadow: ShadowRoot): void {
         shadow.querySelector(`.guess-letter:nth-child(${index + 1})`)?.classList.add(_class);
     }
 
+    /**
+     * This method selects a new word from the wordlist
+     * @returns a new word
+     */
     newWord(){
         this.word = this.selectWord(data);
         return this.word
     }
 }
 
+/**
+ * Alerts the user and reloads the window.
+ */
 function surrender(){
     alert("Das richtige Wort war "+ app.word);
     window.location.reload();
